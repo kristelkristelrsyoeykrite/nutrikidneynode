0f79e861cf8eefcce64fbfe91786f1cf7299c194
kristelkristelrsyoeykrite <154490454+kristelkristelrsyoeykrite@users.noreply.github.com>
2026-05-29 14:27:33 +0800

    Add iOS Firebase configuration

--- nutri_kidney/ios/Runner/AppDelegate.swift
+++ nutri_kidney/ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import Flutter
 import UIKit
+import FirebaseCore
 
 @main
 @objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate {
@@ -7,7 +8,13 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+
+    FirebaseApp.configure()
+
+    return super.application(
+      application,
+      didFinishLaunchingWithOptions: launchOptions
+    )
   }
 
   func didInitializeImplicitFlutterEngine(_ engineBridge: FlutterImplicitEngineBridge) {
